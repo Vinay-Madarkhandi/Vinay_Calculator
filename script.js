eba025b2a105151d27cf51f41e8a5de33d47c2d1
Madarkhandi Vinay Shantappa <139522065+Vinay-Madarkhandi@users.noreply.github.com>
2023-07-29 18:43:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('calInput');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'del'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '%'){
+            string = "Bhai manually gin le% , uska formula hai divide the value by the total value and then multiply the resultant by 100";
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
