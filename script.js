e865c58b17fa3fd4ec9c9e7a2969a57ddabc41ce
Madarkhandi Vinay Shantappa <139522065+Vinay-Madarkhandi@users.noreply.github.com>
2023-07-29 18:50:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
+    button.addEventListener('click', (val) =>{
+        if(val.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(val.target.innerHTML == 'AC'){
             string = "";
             input.value = string;
         }
-        else if(e.target.innerHTML == 'del'){
+        else if(val.target.innerHTML == 'del'){
             string = string.substring(0, string.length-1);
             input.value = string;
         }
-        else if(e.target.innerHTML == '%'){
+        else if(val.target.innerHTML == '%'){
             string = "Bhai manually gin le% , uska formula hai divide the value by the total value and then multiply the resultant by 100";
             input.value = string;
         }
         else{
-            string += e.target.innerHTML;
+            string += val.target.innerHTML;
             input.value = string;
         }
         
